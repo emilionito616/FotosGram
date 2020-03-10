88259c621c19b619fbb4346a3862fc70f46846eb
emilionito616 <emilionito9@gmail.com>
2020-03-10 16:51:53 -0600

    Preparar la pantalla de actualizar usuario

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -8,6 +8,7 @@ import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
 import { ComponentsModule } from '../../components/components.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/interfaces';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor() {}
+  usuario: Usuario = {};
+
+  constructor( private usuarioservice: UsuarioService) {}
+
+  ngOnInit() {
+
+  }
+
+  logout() {
+
+  }
 
 }
